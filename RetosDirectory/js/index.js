5cd7a399cca3eca48877dcbb921e995ec15a6123
Jcova2003 <jesusecova73@gmail.com>
2025-05-08 02:42:32 -0400

    perfil con información dinámica

--- RetosDirectory/js/index.js
+++ RetosDirectory/js/index.js
@@ -1,26 +1,6 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-  // document.querySelector(".nombre").textContent = config.nombre;
-  const descripcion = document.querySelector(".description");
-  if (descripcion) descripcion.textContent = config.descripcion;  
-
-  const filas = document.querySelectorAll(".table tr");
-  
-  if (filas.length >= 5) {
-  filas[0].children[0].textContent = config.color;
-  filas[1].children[0].textContent = config.libro;
-  filas[2].children[0].textContent = config.musica;
-  filas[3].children[0].textContent = config.video_juego;
-  filas[4].children[0].textContent = config.lenguajes;
-  }
-
-  const emailLink = document.querySelector(".email-container");
-  if (emailLink) {
-    emailLink.textContent = config.email;
-    emailLink.href = `mailto:${config.email}`;
-  }
-
   // Para index.html
   const pageName = document.querySelector(".page-name");
   if (pageName) {
@@ -69,4 +49,65 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 
+  //perfil.html
+
+    // document.querySelector(".nombre").textContent = config.nombre;
+    const descripcion = document.querySelector(".description");
+    if (descripcion) descripcion.textContent = config.descripcion;  
+  
+    const filas = document.querySelectorAll(".table tr");
+    
+    if (filas.length >= 5) {
+    filas[0].children[0].textContent = config.color;
+    filas[1].children[0].textContent = config.libro;
+    filas[2].children[0].textContent = config.musica;
+    filas[3].children[0].textContent = config.video_juego;
+    filas[4].children[0].textContent = config.lenguajes;
+    }
+
 });
+
+if( window.location.pathname.includes("perfil.html")) {
+window.onload = function () {
+  const params = new URLSearchParams(window.location.search);
+  const ci = params.get("ci");
+
+  if (!ci) {
+      document.body.innerHTML = "<h2>CI no especificada en la URL.</h2>";
+      return;
+  }
+
+  const script = document.createElement('script');
+  script.src = `${ci}/perfil.json`;
+  script.onload = function () {
+
+      const filas = document.querySelectorAll(".table tr");
+
+      document.title = perfil.nombre;
+      document.querySelector(".perfil-img").src = `${ci}/${ci}.jpg`;
+      document.querySelector(".nombre").textContent = perfil.nombre;
+      document.querySelector(".description").textContent = perfil.descripcion;
+      filas[0].children[1].textContent  = perfil.color;
+      filas[1].children[1].textContent = perfil.libro;
+      filas[2].children[1].textContent = perfil.musica;
+      filas[3].children[1].textContent = perfil.video_juego;
+      filas[4].children[1].innerHTML = perfil.lenguajes.map(l => `<strong>${l}</strong>`).join(', ');
+     
+      const email = document.querySelector(".email-container");
+      if (email) {
+        email.innerHTML = config.email.replace(
+          "[email]",
+          `<a class="mail" href="mailto:${perfil.email}">${perfil.email}</a>`
+        );
+      }
+      
+
+  };
+
+  script.onerror = function () {
+      document.body.innerHTML = `<h2>No se encontró el perfil para la cédula: ${ci}</h2>`;
+  };
+
+  document.body.appendChild(script);
+}
+};
